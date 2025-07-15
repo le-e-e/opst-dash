72288c658d4f1448127a1cfabda2abb13435e1ae
이승재 <lee@iseungjaeui-MacBookPro.local>
2025-07-15 18:42:50 +0900

    cors-clear

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,10 +1,10 @@
 export const OPENSTACK_ENDPOINTS = {
-  KEYSTONE: 'http://controller:5000/v3/',
-  NOVA: 'http://controller:8774/v2.1',
-  NEUTRON: 'http://controller:9696',
-  GLANCE: 'http://controller:9292',
-  CINDER: 'http://controller:8776/v3/e70a1557498a46e08839fdfb88fd9a1d',
-  PLACEMENT: 'http://controller:8778'
+  KEYSTONE: '/keystone/',
+  NOVA: '/nova',
+  NEUTRON: '/neutron',
+  GLANCE: '/glance',
+  CINDER: '/cinder',
+  PLACEMENT: '/placement'
 } as const;
 
 export const AUTH_CONFIG = {
@@ -13,7 +13,7 @@ export const AUTH_CONFIG = {
   PROJECT_NAME: 'admin',
   USERNAME: 'admin',
   PASSWORD: '292747187bb383f8fe73',
-  AUTH_URL: 'http://controller:5000/v3',
+  AUTH_URL: '/keystone',
   IDENTITY_API_VERSION: '3',
   IMAGE_API_VERSION: '2'
 } as const; 
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,78 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true
+    host: true,
+    proxy: {
+      // Keystone API 프록시
+      '/keystone': {
+        target: 'http://controller:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/keystone/, '/v3'),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Keystone proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Keystone proxy request:', req.method, req.url);
+          });
+        }
+      },
+      // Nova API 프록시
+      '/nova': {
+        target: 'http://controller:8774',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/nova/, '/v2.1'),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Nova proxy error', err);
+          });
+        }
+      },
+      // Neutron API 프록시
+      '/neutron': {
+        target: 'http://controller:9696',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/neutron/, ''),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Neutron proxy error', err);
+          });
+        }
+      },
+      // Glance API 프록시
+      '/glance': {
+        target: 'http://controller:9292',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/glance/, ''),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Glance proxy error', err);
+          });
+        }
+      },
+      // Cinder API 프록시
+      '/cinder': {
+        target: 'http://controller:8776',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/cinder/, '/v3/e70a1557498a46e08839fdfb88fd9a1d'),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Cinder proxy error', err);
+          });
+        }
+      },
+      // Placement API 프록시
+      '/placement': {
+        target: 'http://controller:8778',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/placement/, ''),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Placement proxy error', err);
+          });
+        }
+      }
+    }
   },
   build: {
     outDir: 'dist',
